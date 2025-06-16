23b1990ba2ee265dcd1fd6b7c6fbc878318ce051
Zandersan <87540372+Zandersan@users.noreply.github.com>
2025-06-16 17:48:07 -0300

    Update reserva.py

--- reserva.py
+++ reserva.py
@@ -116,7 +116,12 @@ def enviar_email_com_confirmacao(cpf, nome, data, horario):
 
 def tentar_reserva(cpf, senha, data_domingo):
     chrome_options = Options()
-    chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"')
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
     driver = webdriver.Chrome(options=chrome_options)
     
     nome = get_nome_por_cpf(cpf)
